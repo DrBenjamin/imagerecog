d580e560cae29cb2d32190b05d68e4c8c5cf86ca
DrBenjamin <drdrbenjamin@icloud.com>
2025-05-15 18:31:43 +0200

    minor improvements

--- src/globals.py
+++ src/globals.py
@@ -15,12 +15,12 @@ file_path = None # storing the path of the selected file
 
 # Setting MinIO credentials from wx.Config (user preferences) or environment variables
 config = wx.Config("Dateiablage")
-minio_endpoint = config.Read("minio_endpoint", os.environ.get("MINIO_ENDPOINT", "212.227.102.172:9000"))
+minio_endpoint = config.Read("minio_endpoint", os.environ.get("MINIO_ENDPOINT", "127.0.0.1:9000"))
 minio_access_key = config.Read("minio_access_key", os.environ.get("MINIO_ACCESS_KEY", "<username>"))
 minio_secret_key = config.Read("minio_secret_key", os.environ.get("MINIO_SECRET_KEY", "<password>"))
 minio_secure = config.ReadBool("minio_secure", os.environ.get("MINIO_SECURE", "False") == "True")
 minio_bucket_name = config.Read("minio_bucket_name", os.environ.get("MINIO_BUCKET", "<bucketname>"))
 
 # Setting embedded Streamlit credentials from wx.Config (user preferences)
-streamlit_endpoint = config.Read("streamlit_url", "localhost:8501")
+streamlit_endpoint = config.Read("streamlit_url", "127.0.0.1:8501")
 streamlit_secure = config.ReadBool("streamlit_secure", False)
